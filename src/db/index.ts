86e317bfa1eafc4f29cc8f4248c0a3f80247984f
letruxux <mrgianfranco483@gmail.com>
2026-04-03 12:12:19 +0200

    fix: use name in env, schema enhancements

--- src/db/index.ts
+++ src/db/index.ts
@@ -1,7 +1,8 @@
 import { drizzle } from "drizzle-orm/bun-sqlite";
 import * as schema from "./schema";
+import { env } from "@/env";
 
-export const db = drizzle("guilds.db", { schema });
+export const db = drizzle(env.DATABASE_FILENAME, { schema });
 
 export async function getTokenOfUser(userId: string) {
   const token = await db.query.userTokens.findFirst({
--- src/env.ts
+++ src/env.ts
@@ -3,12 +3,12 @@ import * as z from "zod";
 
 export const env = createEnv({
   server: {
-    DATABASE_FILENAME: z.string().min(1),
+    DATABASE_FILENAME: z.string().min(1).default("linear.sqlite"),
     FLUXER_TOKEN: z.string().min(1),
     PORT: z.coerce.number().default(8288),
     LINEAR_CLIENT_ID: z.string().min(1),
     LINEAR_CLIENT_SECRET: z.string().min(1),
-    LINEAR_REDIRECT_URI: z.string().url(),
+    LINEAR_REDIRECT_URI: z.url(),
   },
 
   runtimeEnv: process.env,
