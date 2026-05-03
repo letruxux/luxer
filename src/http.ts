29cb7d4782eca730c3f5234f0c9773f3eafabea0
letruxux <mrgianfranco483@gmail.com>
2026-05-03 18:22:41 +0200

    feat: ver in http

--- src/http.ts
+++ src/http.ts
@@ -6,6 +6,8 @@ export default function makeHTTP() {
   const webhookRoutes = makeWebhookRoutes();
   const http = new Hono();
 
+  http.get("/", (c) => c.text("Running Luxer " + require("@/../package.json").version));
+
   http.route("/callback", authRoutes);
   if (webhookRoutes) http.route("/webhook", webhookRoutes);
 
